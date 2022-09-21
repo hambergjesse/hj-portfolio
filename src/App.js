ce5ff129f302abaf9cbbd3d4236f4367e656f2d0
Jesse Hamberg <hambergjesse@gmail.com>
2022-09-21 14:55:44 +0300

    session end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Background from "./components/Background";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import "./scss/index.scss";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
-    <div className="app__wrapper">
-      <Header />
-      <Content />
-      <Background />
-    </div>
+    !loading && (
+      <div className="app__wrapper">
+        <Header />
+        <Content />
+        <Background />
+      </div>
+    )
   );
 };
 
